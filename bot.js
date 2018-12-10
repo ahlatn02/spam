8ac6a10a1f78b2e7e7769e34df5da4ce6e903a6b
ahlatn02 <44965902+ahlatn02@users.noreply.github.com>
2018-12-10 15:30:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,8 @@ client.on("message", async message => {
         // random sentence by pulling words from a dictionary file, or to just send a random
         // arrangement of characters and integers. Doing something like this may help prevent
         // future bots from detecting that you sent a spam message.
-        message.channel.send(possible.charAt(Math.floor(Math.random() * possible.length)) + "Số thứ tự của bạn là" + count);
+        message.channel.send("Số thứ tự của bạn là" + count);
+          //possible.charAt(Math.floor(Math.random() * possible.length)) 
 
         if (count < maxMessages) {
           // If you don't care about whether the messages are deleted or not, like if you created a dedicated server
